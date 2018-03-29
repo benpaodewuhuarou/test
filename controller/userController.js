3eb8b589b3bf276167c904af0213ebba0d1684d1
shizekang <shizekang1@gmail.com>
2018-03-28 21:08:55 -0400

    delete some files

--- controller/userController.js
+++ controller/userController.js
@@ -7,6 +7,15 @@ var logger = getLoggerTool.getLogger();
 
 /**
  * add user, the input's type should be the json
+ * "username" : "1qq11", (must)
+ * "email" : "658939539@qq.com", (must)
+ * "password" : "111111111111111111", (must)
+ * "address" : "",
+ * "phoneNumber" : "",
+ * "wechat" : "",
+ * "restrict" : "false", (initial value: false)
+ * "credit" : "100", (initial value: 100)
+ * "administrator" : "false", (initial value: false)
  * @param user
  * @returns {Promise.<void>}
  */
@@ -33,7 +42,7 @@ async function existUser(username) {
     try {
         var exist = await userService.existUser(username);
         result["status"] = 200;
-        result["message"] = exist;
+        result["message"] = exist; //if the username exists, it means: result["message"] = true
     } catch (e) {
         logger.error("existUser in user Controller error: " + e);
         result["status"] = 400;
@@ -52,7 +61,7 @@ async function existEmail(email) {
     try {
         var exist = await userService.existEmail(email);
         result["status"] = 200;
-        result["message"] = exist;
+        result["message"] = exist; //if the email exists, it means: result["message"] = true
     } catch (e) {
         logger.error("existEmail in user Controller error: " + e);
         result["status"] = 400;
